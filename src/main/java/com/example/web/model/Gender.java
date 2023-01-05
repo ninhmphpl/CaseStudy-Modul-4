9a916ef20c5b1bea2d89363bd516d3b84ad6c2ee
NinhPc <ninhmphpl@gmail.com>
2023-01-05 10:15:41 +0700

    add first

--- src/main/java/com/example/web/model/Gender.java
+++ src/main/java/com/example/web/model/Gender.java
@@ -7,13 +7,14 @@ import lombok.NoArgsConstructor;
 import javax.persistence.*;
 
 @Entity
-@Table(name = "skills")
+@Table(name = "genders")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Skill {
+public class Gender {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
     @Column(name = "name")
     private String name;
