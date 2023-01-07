ea3dc77aa69231da831253d655ff8bd99bad5f8b
ADMIN <taiphiets666@gmail.com>
2023-01-07 17:18:02 +0700

    Hết Ngày m7

--- src/main/java/com/example/web/controller/OfferController.java
+++ src/main/java/com/example/web/controller/OfferController.java
@@ -36,11 +36,6 @@ public class OfferController {
     public ResponseEntity<List<Offer>> findSearch(@RequestParam("search") String search){
         return new ResponseEntity<>(offerService.findAllByName(search) , HttpStatus.OK);
     }
-    @GetMapping("/{id}")
-    public ResponseEntity<Offer> findById(@PathVariable Long id){
-        return new ResponseEntity<>(offerService.findById(id).get(),HttpStatus.OK);
-    }
-
 }
 
 
--- src/main/resources/templates/themes.TechJobs/client/js/admin-show-offer/form.js
+++ src/main/resources/templates/themes.TechJobs/client/js/admin-show-offer/form.js
@@ -10,21 +10,18 @@ function searchByName(){
                 let description = data[i].description
                 let city = data[i].city
                 let amount = data[i].amount
-                let workExperience = data[i].workExperience
-                let skill = data[i].skill
                 let endDate = data[i].endDate
                 let name = data[i].name
                 let status = data[i].status
-                let company = data[i].company
                 let id = data[i].id
                 // (id , name, description,career, endDate , city, amount,workExperience,skill, status,company)
-                content += form(id, name ,description, career , endDate , city , amount,workExperience, skill , status ,company )
+                content += form(id, name ,description, career , endDate , city , amount , status )
                 document.getElementById("findAll").innerHTML = content;
             }
         }
     })
 }
-function form(id , name, description,career, endDate , city, amount,workExperience,skill, status,company) {
+function form(id , name, description,career, endDate , city, amount, status) {
     return ` <div class="job pagi">
               <div class="job-content">
                 <div class="job-logo">
@@ -37,27 +34,19 @@ function form(id , name, description,career, endDate , city, amount,workExperien
                     <span>${name}</span>
                   </div>
                   <div class="job-company">
-                    <span href="">${description}</span>
+                    <span href="">Mô Tả:${description}</span>
                   </div>
                   <div class="job-company">
-                    <span href="">${career}</span>
+                    <span href="">Nghề Nghiệp:${career}</span>
                   </div>
                   <div class="job-company">
-                    <span href="">${endDate}</span>
+                    <span href="">Hạn Nộp Hồ Sơ:${endDate}</span>
                   </div>
                   <div class="job-company">
-                    <span href="">${city}</span>
+                    <span href="">Thành Phố:${city}</span>
                   </div>
                   <div class="job-company">
-                    <span href="">${amount}</span>
-                  </div><div class="job-company">
-                    <span href="">${workExperience}</span>
-                  </div><div class="job-company">
-                    <span href="">${skill}</span>
-                  </div><div class="job-company">
-                    <span href="">${status}</span>
-                  </div><div class="job-company">
-                    <span href="">${company}</span>
+                    <span href="">Số Lượng:${amount}</span>
                   </div>
                   <div class="job-company">
                     Trạng thái: ${statusRender(status.id)}
@@ -90,19 +79,16 @@ function renderForm() {
         success: function (data) {
             let content = ""
             for (let i = 0; i < data.length; i++) {
-                let career = data[i].career
+                let career = data[i].career.name
                 let description = data[i].description
-                let city = data[i].city
+                let city = data[i].city.name
                 let amount = data[i].amount
-                let workExperience = data[i].workExperience
-                let skill = data[i].skill
                 let endDate = data[i].endDate
                 let name = data[i].name
                 let status = data[i].status
-                let company = data[i].company
                 let id = data[i].id
                 // (id , name, description,career, endDate , city, amount,workExperience,skill, status,company)
-                content += form(id, name ,description, career , endDate , city , amount,workExperience, skill , status ,company )
+                content += form(id, name ,description, career , endDate , city , amount , status  )
                 document.getElementById("findAll").innerHTML = content;
             }
         }
