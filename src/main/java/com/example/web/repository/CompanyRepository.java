a8af0c422e73fd76478e57f0a61de419c1257d49
NinhPc <ninhmphpl@gmail.com>
2023-01-08 08:33:17 +0700

    ninh comit singIn

---
+++ src/main/java/com/example/web/repository/CompanyRepository.java
@@ -0,0 +1,9 @@
+package com.example.web.repository;
+
+import com.example.web.model.Company;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CompanyRepository extends JpaRepository<Company,Long> {
+}
