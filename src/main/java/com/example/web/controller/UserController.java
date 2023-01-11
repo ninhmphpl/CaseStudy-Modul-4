e7ae0ca6e446da50b18d448ea4f164e2b715421f
ADMIN <taiphiets666@gmail.com>
2023-01-11 11:13:37 +0700

    Big update

--- src/main/java/com/example/web/controller/UserController.java
+++ src/main/java/com/example/web/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.example.web.controller;
 
+import com.example.web.model.Offer;
 import com.example.web.model.User;
 import com.example.web.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -23,5 +25,9 @@ public class UserController {
     public ResponseEntity<List<User>> findAll() {
 
         return new ResponseEntity<>(userService.findAll(), HttpStatus.OK);
+    }
+    @GetMapping("/searchEmail")
+   public ResponseEntity<?>findByEmailName(@RequestParam ("searchEmail") String searchEmail){
+       return new ResponseEntity<>(userService.findUserEmail(searchEmail) , HttpStatus.OK);
     }
 }
